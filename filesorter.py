acf4e335d329e24e9436e114ffdb5d4018ea93b9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-10-22 12:59:25 +0200

    Basic argument parsing.

--- filesorter.py
+++ filesorter.py
@@ -22,34 +22,21 @@ The help message goes here.
 '''
 
 
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
+def sort_files(path, dry_run=False):
+    if dry_run:
+        print 'dry run'
+    print path
 
 def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    try:
-        try:
-            opts, args = getopt.getopt(argv[1:], "ho:v", ["help", "output="])
-        except getopt.error, msg:
-            raise Usage(msg)
-    
-        # option processing
-        for option, value in opts:
-            if option == "-v":
-                verbose = True
-            if option in ("-h", "--help"):
-                raise Usage(help_message)
-            if option in ("-o", "--output"):
-                output = value
-    
-    except Usage, err:
-        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
-        print >> sys.stderr, "\t for help use --help"
-        return 2
+    import argparse
 
+    parser = argparse.ArgumentParser(description='Sort and order the files in a directory by date and filetype.')
+    parser.add_argument('path', metavar='<pathname>', type=str, )
+    parser.add_argument('--dry-run', help='Show what would have been done.', action='store_true')
+
+    args = parser.parse_args()
+
+    sort_files(args.path, args.dry_run)
 
 if __name__ == "__main__":
     sys.exit(main())
