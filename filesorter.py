01a61336fa32d5f50aeadd1c32633feb510b1883
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-10-22 14:10:47 +0200

    Progess

--- filesorter.py
+++ filesorter.py
@@ -10,18 +10,28 @@ This program is free software. It comes without any warranty, to
 the extent permitted by applicable law. You can redistribute it
 and/or modify it under the terms of the Do What The Fuck You Want
 To Public License, Version 2, as published by Sam Hocevar. See
-COPYING for more details. 
+COPYING for more details.
 """
 
-import sys, argparse, os, re, logging, mimetypes
+import sys, argparse, os, re, logging, mimetypes, shutil, time
+from datetime import datetime, timedelta
 
-
+# Default logger
 logger = logging.getLogger('filesorter')
 
-
+# Ignore these files
 IGNORE_FILENAMES = (
     re.compile('^\.'),
 )
+
+# Explicitly add AVCHD
+mimetypes.add_type('video/avchd', '.mts')
+mimetypes.add_type('video/avchd', '.m2ts')
+
+NEW_PATH = '%(date)s/%(type)s/%(orig_path)s/%(filename)s'
+
+MIN_AGE = timedelta(minutes=5)
+
 
 def ignore_filename(filename):
     """ Whether or not to ignore a specific path. """
@@ -31,36 +41,52 @@ def ignore_filename(filename):
     return False
 
 
-def sort_files(path, dry_run=False):
+def sort_files(path, dest, dry_run=False):
+    now = datetime.now()
+
     logger.info('Processing %s', path)
 
     if dry_run:
         print 'dry run'
 
     for (dirpath, dirnames, filenames) in os.walk(path):
-        logger.debug('dirpath: %s, dirnames: %s, filenames: %s', dirpath, dirnames, filenames)
-        
+        # logger.debug('dirpath: %s, dirnames: %s, filenames: %s', dirpath, dirnames, filenames)
+
+        dirmtime = datetime.fromtimestamp(os.path.getmtime(dirpath))
+
+        delta = now - dirmtime
+        if delta < MIN_AGE:
+            logger.warn('Directory %s modified too recently, skipping (%s minute(s) ago)',
+                        dirpath, int(delta.total_seconds()/60))
+            continue
+
         for filename in filenames:
             if ignore_filename(filename):
                 logger.info('Ignoring %s', filename)
                 continue
-        
-            logger.debug('Processing %s', filename)
-        
+
+            fullpath = os.path.join(dirpath, filename)
+            logger.debug('Processing: %s', fullpath)
+
             (mimetype, enoding) = mimetypes.guess_type(filename)
-            
+
             if not mimetype:
                 logger.warn('Mimetype for %s could not be determined', filename)
             else:
                 logger.debug('Mimetype: %s', mimetype)
-            
+
             main_type = mimetype.split('/')[0].title()
             logger.debug('Main type: %s', main_type)
-    
+
+            mtime = datetime.fromtimestamp(os.path.getmtime(fullpath))
+            # if not dry_run:
+            #     shutil.move(src, dst)
+
 
 def main(argv=None):
     parser = argparse.ArgumentParser(description='Sort and order the files in a directory by date and filetype.')
     parser.add_argument('path', metavar='<pathname>', type=str)
+    parser.add_argument('--dest', '-d', metavar='<dest>', type=str)
     parser.add_argument('--dry-run', '-n', help='Show what would have been done.', action='store_true')
     parser.add_argument('--logging', '-l', help='Log level.', type=str, default='info', choices=('debug', 'info', 'warn', 'error'))
     args = parser.parse_args()
@@ -68,7 +94,7 @@ def main(argv=None):
     numeric_level = getattr(logging, args.logging.upper())
     logging.basicConfig(level=numeric_level, format='%(message)s')
 
-    sort_files(args.path, args.dry_run)
+    sort_files(args.path, args.dry_run, args.dest or args.path)
 
 if __name__ == "__main__":
     sys.exit(main())
